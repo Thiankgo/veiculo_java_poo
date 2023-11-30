9ef21c9d5e6f02b68873328e3ab4f45a0f7ca1ad
thiago.paiva <tdesousapaiva@gmail.com>
2023-11-30 01:01:12 -0300

    feat: added gui

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static DefaultListModel<Veiculo> listModel;
+
+    static private JComboBox<String> listaVeiculos;
+
+    static private String currentVehicle;
+    static private JTextField nomeText, placaText, anoText;
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        listModel = new DefaultListModel<>();
+        JList<Veiculo> veiculoList = new JList<>(listModel);
+        veiculoList.setCellRenderer(new VeiculoCellRenderer());
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        listaVeiculos = new JComboBox<>(new String[]{"Carro", "Onibus", "Caminhão"});
+        listaVeiculos.setBounds(300, 10, 160, 25);
+        listaVeiculos.addActionListener(e -> updateVehicle((String) listaVeiculos.getSelectedItem()));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        JButton addButton = getButton();
+
+        JScrollPane scrollPane = new JScrollPane(veiculoList);
+        scrollPane.setBounds(0, 200, 600, 1000);
+
+        JFrame frame = new JFrame("Lista de Veículos");
+        frame.setLayout(new BorderLayout());
+        frame.add(listaVeiculos);
+        frame.add(addButton);
+        frame.add(scrollPane);
+        frame.setSize(600, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        frame.setVisible(true);
+    }
+
+    private static JButton getButton() {
+        JButton addButton = new JButton("Adicionar Veículo");
+        addButton.setBounds(10, 10, 200, 25);
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Adiciona um novo veículo à lista
+                Veiculo novoVeiculo = new Veiculo("Modelo " + (listModel.getSize() + 1),
+                        "ABC123" + (listModel.getSize() + 1),
+                        2022);
+                listModel.addElement(novoVeiculo);
+            }
+        });
+        return addButton;
+    }
+    static private void updateVehicle(String tipoVeículo) {
+        currentVehicle = tipoVeículo;
     }
 }
--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,10 +1,23 @@
+import java.awt.*;
+
 public class Veiculo {
+    protected String nome;
     protected String placa;
+    protected  String image = "img/car.png";
     protected int ano;
 
-    public Veiculo(String placa, int ano){
+    public Veiculo(String nome, String placa, int ano) {
+        this.nome = nome;
         this.placa = placa;
         this.ano = ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getPlaca() {
@@ -23,5 +36,8 @@ public class Veiculo {
         this.ano = ano;
     }
 
-
+    public void desenhar(Graphics g) {
+        Image imagem = Toolkit.getDefaultToolkit().getImage("img/car.png");
+        g.drawImage(imagem, 500, 500, 500, 500, null);
+    }
 }
